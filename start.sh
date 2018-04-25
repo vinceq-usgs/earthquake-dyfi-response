020f6fd8540fcd1513039921066265a7b3ea50b8
Jeremy Fee <jmfee@usgs.gov>
2018-04-25 13:27:16 -0600

    Mount src directories in development container, move to start.sh script

---
+++ start.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+CONTAINER_PORT=${CONTAINER_PORT:-8123}
+
+
+echo "Starting container on http://localhost:${CONTAINER_PORT}/"
+echo
+
+docker run --rm \
+    -p ${CONTAINER_PORT}:80 \
+    -v $(pwd)/responses:/data \
+    -v $(pwd)/src/conf:/var/www/conf \
+    -v $(pwd)/src/htdocs:/var/www/html \
+    -v $(pwd)/src/lib:/var/www/lib \
+    usgs/earthquake-dyfi-response:latest
