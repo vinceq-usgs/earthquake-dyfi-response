afa1046df33cfa880d9ec2645ee2ed8f172a2aff
Jeremy Fee <jmfee@usgs.gov>
2018-04-27 10:16:52 -0600

    Fix what appears to be broken geocoding

--- src/htdocs/response.php
+++ src/htdocs/response.php
@@ -49,19 +49,23 @@ $time = time();
 
 $form_version = isset($_POST['form_version']) ? $_POST['form_version'] : null;
 $language = isset($_POST['language']) ? $_POST['language'] : 'en';
+$mapAddress = isset($_POST['ciim_mapAddress']) ? $_POST['ciim_mapAddress'] : null;
 
 // Do we need to do a geocode?
-$mapAddress = isset($_POST['ciim_mapAddress']) ? $_POST['ciim_mapAddress'] : null;
 if ($mapAddress !== null) {
-	include_once('../lib/geocode.php');
-	try {
-		$client_id = $CONFIG['ARCGIS_CLIENT_ID'];
-	        $client_secret = $CONFIG['ARCGIS_CLIENT_SECRET'];
-		geocode($client_id,$client_secret);
-	} catch (Exception $ex) {
+  include_once('../lib/geocode.php');
+  try {
+    $client_id = $CONFIG['ARCGIS_CLIENT_ID'];
+    $client_secret = $CONFIG['ARCGIS_CLIENT_SECRET'];
+    $result = geocode($client_id, $client_secret, $mapAddress);
+    if ($result) {
+      $_POST['ciim_mapLat'] = $result['latitude'];
+      $_POST['ciim_mapLon'] = $result['longitude'];
+    }
+  } catch (Exception $ex) {
     // Oh well, we tried...
     trigger_error($ex->getMessage());
-	}
+  }
 }
 
 
@@ -112,7 +116,7 @@ foreach ($files as $dest) {
 
 $translate_file = 'inc/labels.' . $language . '.inc.php';
 if (!file_exists($translate_file)) {
-	$translate_file = 'inc/labels.en.inc.php';
+  $translate_file = 'inc/labels.en.inc.php';
 }
 include_once($translate_file);
 
@@ -121,7 +125,7 @@ $data = $_POST;
 $data['eventid'] = $eventid;
 $cdi = _rom(compute_intensity());
 if (isset($cdi)) {
-	$data['your_cdi'] = $cdi;
+  $data['your_cdi'] = $cdi;
 }
 
 // Lookup of other entries is disabled for now
@@ -131,42 +135,42 @@ if (isset($cdi)) {
 //}
 
 $OUTPUT = array (
-	'eventid' => $T['EVENTID_LABEL'],
-	'your_cdi' => $T['ESTIMATED_II_LABEL'],
-	'all_cdi' => $T['COMMUNITY_II_LABEL'],
-	'nresp' => $T['NRESP_LABEL'],
-	'ciim_zip' => $T['ZIPCODE_LABEL'],
-	'ciim_city' => $T['CITY_LABEL'],
-	'ciim_region' => $T['REGION_LABEL'],
-	'ciim_country' => $T['COUNTRY_LABEL'],
-	'fldContact_name' => $T['NAME_LABEL'],
-	'fldContact_email' => $T['EMAIL_LABEL'],
-	'fldContact_phone' => $T['PHONE_LABEL'],
-	'ciim_address' => $T['ADDRESS_LABEL'],
-	'ciim_time' => $T['EVENTTIME_LABEL'],
-	'ciim_mapLat' => 'Latitude',
-	'ciim_mapLon' => 'Longitude',
+  'eventid' => $T['EVENTID_LABEL'],
+  'your_cdi' => $T['ESTIMATED_II_LABEL'],
+  'all_cdi' => $T['COMMUNITY_II_LABEL'],
+  'nresp' => $T['NRESP_LABEL'],
+  'ciim_zip' => $T['ZIPCODE_LABEL'],
+  'ciim_city' => $T['CITY_LABEL'],
+  'ciim_region' => $T['REGION_LABEL'],
+  'ciim_country' => $T['COUNTRY_LABEL'],
+  'fldContact_name' => $T['NAME_LABEL'],
+  'fldContact_email' => $T['EMAIL_LABEL'],
+  'fldContact_phone' => $T['PHONE_LABEL'],
+  'ciim_address' => $T['ADDRESS_LABEL'],
+  'ciim_time' => $T['EVENTTIME_LABEL'],
+  'ciim_mapLat' => 'Latitude',
+  'ciim_mapLon' => 'Longitude',
 
-	'filename' => "Output",
-	'form_version' => "Form version",
+  'filename' => "Output",
+  'form_version' => "Form version",
 );
 
 $OUTPUT_DATA = array();
 foreach($OUTPUT as $key => $desc) {
-	if (!array_key_exists($key, $data)) {
-		continue;
-	}
+  if (!array_key_exists($key, $data)) {
+    continue;
+  }
 
-	$val = $data[$key];
-	if (!$val) {
-		continue;
-	}
+  $val = $data[$key];
+  if (!$val) {
+    continue;
+  }
 
-	if ($key == 'ciim_city' || $key == 'ciim_region' || $key == 'ciim_country') {
-		$val = _strip_code($val);
-		if (!$val) {
-			continue;
-		}
+  if ($key == 'ciim_city' || $key == 'ciim_region' || $key == 'ciim_country') {
+    $val = _strip_code($val);
+    if (!$val) {
+      continue;
+    }
   }
 
   $OUTPUT_DATA[$key] = $val;
@@ -196,9 +200,9 @@ echo '<p class="alert success">' . $T['THANKS_LABEL'] . '</p>';
 echo '<dl>';
 
 foreach ($OUTPUT_DATA as $key => $val) {
-	// Loop over results and append the rows
-	echo '<dt>' . $OUTPUT[$key] . '</dt>';
-	echo '<dd>' . htmlspecialchars($val) . '</dd>';
+  // Loop over results and append the rows
+  echo '<dt>' . $OUTPUT[$key] . '</dt>';
+  echo '<dd>' . htmlspecialchars($val) . '</dd>';
 }
 
 echo '</dl>';
