4ff73842c32e01ba33906bcd322d8935f14fc447
Jeremy Fee <jmfee@usgs.gov>
2018-04-30 11:50:53 -0600

    Add healthcheck script

---
+++ src/lib/healthcheck.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+## Health check script for docker container
+# Check for recent responses.
+# If no recent responses are found, send one and check again.
+
+
+BACKUP_DIR=${BACKUP_DIR:-'/backup'}
+RESPONSES_DIR="${BACKUP_DIR}/responses"
+if [ ! -d $RESPONSES_DIR ]; then
+  mkdir -p $RESPONSES_DIR
+  chmod 777 $RESPONSES_DIR
+fi
+
+
+# check for responses within the past 5 minutes
+num_responses=$(find $RESPONSES_DIR -mmin -5 -name '*' -type f | wc -l)
+echo "Found ${num_responses} responses in past 5 minutes"
+if [ "$num_responses" != "0" ]; then
+  exit 0
+fi
+
+
+# no response found, send one
+echo "Submitting response"
+status=$(
+    curl \
+        --request POST \
+        --data "eventid=healthcheck&fldSituation_felt=0&format=json" \
+        --max-time 5 \
+        --output /dev/null \
+        --silent \
+        --write-out "%{http_code}" \
+        http://localhost/response.php
+)
+if [ "$status" != "200" ]; then
+  echo "Error submitting response, expected 200 got ${status}"
+  exit 1
+fi
+
+# expect the response we just sent to be found
+num_responses=$(find $RESPONSES_DIR -mmin -5 -name '*' -type f | wc -l)
+if [ "$num_responses" != "0" ]; then
+  echo "Response submitted successfully"
+  exit 0
+fi
+echo "Response was not output as expected"
+exit 1
